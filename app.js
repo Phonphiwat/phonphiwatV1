04fb9add46c6b7cdd000798a403512a6d5a5859a
phonphiwat <phonphiwat.k@suriyait.com>
2022-11-18 10:25:02 +0700

    std JavaScript-html-css

---
+++ app.js
@@ -0,0 +1,174 @@
+
+function testCSS(){
+    alert("Coming soon");
+}
+
+function testHTML(){
+    alert("Coming soon");
+}
+function testJavaScript(){
+
+    //แจ้งเตือนรายระเอียด
+    alert("การใช้คำสั่ง JavaScript"); 
+    alert("Hello User ");
+
+    let cons = [1 ,3,5 ,2 , 4 ];
+    let con = cons.length;
+    document.write("ก่อนเรียง = ",cons);
+    console.log(cons);
+    document.write("<br>เรียงตังเลข = ", cons.sort());
+    console.log(cons);
+
+    let acsr = ["a","e","b","g","d","s" ];
+    let lol = acsr.length;
+    document.write("<br> ก่อนตัวอักษร = ",acsr);
+    console.log(acsr);
+    let result = acsr.sort();
+    document.write("<br> เรียงตัวอักษร = ",result);
+    console.log(result);
+
+    ///////////////////////////////////////////////////////
+    const tax = 0.07;
+let name;
+let Money = 120+1;
+let price = 29.11;
+Name = "phon ";
+gg = 23;
+
+
+//Text
+document.write(" <h2><i>Bam Phonphiwat</i></h2>");
+document.write("<h2><p><i>Kongthong</i></p></h2>");
+document.write(Name, gg);
+//Array
+let num = Array("1","2","3","4","5");
+let color =[" <br> red"," blue ","  black <br> "," yellow "];
+
+document.write("<br> ตัวแรก ",num[0]," ตัวที่สาม ",num[2]);
+document.write(" ตัวที่สอง ",num[1]);
+document.write(color [0] ,color[1],color[2]);
+document.write("<p> A=5 B=2</p>");
+//บวก ลบ คูณ หาร หาเศษ
+
+const A = 5;
+const B = 2;
+
+document.write("ผลบวก= ",A+B);
+document.write(" ผลคูณ= ",A*B);
+document.write(" ผลหาร= ",A/B);
+document.write(" ผลหาเสษ=",A%B);
+//เปรียบเที่ยบ
+document.write("<br> Aเท่ากับBหรือไม่= ",A==B);
+document.write("<br> Aไม่เท่ากับBหรือไม่= ",A!=B);
+document.write("<br> Aน้อยกว่าBหรือไม่= ",A<=B);
+document.write("<br> Aมากกว่าBหรือไม่= ",A>=B,"<br>");
+
+//แจ้งระดับชั้น
+let ae = 15;
+
+if(ae<=15){
+    if(ae==15){
+        document.write("<br>ม.3 <br> Aeg <= 15 <br>");
+    }else if(ae==14){
+        document.write("<br>ม.2 <br> Aeg <= 14 <br>");
+    }else if(ae==13){
+        document.write("<br>ม.1 <br> Aeg <= 13 <br>");
+    }else if(ae<=12){
+        document.write("<br>ประถม <br> Aeg <= 12 <br>");
+    }  
+}
+else{
+    document.write("<br>ม.ปลาย <br> Aeg >= 15 ");
+}
+
+//เงื่อนไขเกรด
+let score = 71 ;
+
+if(score>100){
+    document.write("เกรด = A <br>");
+}
+else if(score>70){
+    document.write("เกรด = B <br>");
+}
+else if(score>60){
+    document.write("เกรด = C <br>");
+}
+else if(score>50){
+    document.write("เกรด = D <br>");
+}
+else if(score<49){
+    document.write("เกรด = F <br>");
+}
+
+
+//ถอนเงิน
+$ = 5000;
+withDraw = 5000;
+
+if(withDraw<=$ ){
+    document.write("<br>ยอดที่ใช้ได้ = ",$);
+    document.write("<br>ดำเนินการถอนเงิน = ",withDraw);
+    $ = $-withDraw;
+    document.write("<br>ยอดคงเหลือ =  ",$,"<br>");
+}
+else{
+    document.write("<br>ยอดเงินไม่เพียงพอ  ");
+}
+
+//ใช้switch กำหนดเงื่อนไข เดือน
+let month = 4
+switch(month){
+    case 1: document.write("<br>มกราคม  ");
+    break;
+    case 2: document.write(" <br>กุมภา  ");
+    break;
+    case 3: document.write(" <br>มีนา  ");
+    break;
+    case 4: document.write(" <br>เมษา  ");
+    break;
+    default: document.write("no info  ");
+
+}
+//การใช้for
+//นับ 0-12
+let i;
+for (i= 0; i<13; i++) {
+  document.write("",i,"<br>");
+}
+//เช็ค arrey โดยใช้ for
+const fr = ["Apple, ", "Banana, ", "Orange, <br>"];
+for (x of fr) {
+  document.write("fruits = ",x);
+}
+//การใช้while
+let count=1
+while(count<=6){
+document.write("<br>Hello",count,"<br>");
+  if(count==3){
+    break;
+}
+count++;
+}
+
+console.log("Hello");
+console.error("ไวรัส");
+console.warn("คำเตือน");
+
+console.log(Name);
+console.log("อายุ = ",gg);
+console.log("vat =",tax);
+
+console.log(Money);
+console.log(price);
+
+}
+
+
+
+
+
+
+
+
+
+
